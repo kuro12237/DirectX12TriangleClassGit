2b4f4540c89fb54a397df61a7a19ebfa1bc29415
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-06-05 15:11:07 +0900

    Revert "Revert "Revert "save"""
    This reverts commit fd32fa7931a9245303e3d585975cdb7b7a3bf096.

--- DirectXSetup.h
+++ DirectXSetup.h
@@ -19,8 +19,8 @@ struct  Vec4
 };
 struct  VertexProperty
 {
-	D3D12_VERTEX_BUFFER_VIEW BufferView;
-	ID3D12Resource* Resource;
+	D3D12_VERTEX_BUFFER_VIEW vertex;
+	ID3D12Resource* resouce;
 };
 
 struct Commands
