f88e968b2511aba59dd994c42482aede11564a6a
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-07-17 16:52:37 +0900

    02_04_確認課題

--- CLEYERA/LogManager/Log.cpp
+++ CLEYERA/LogManager/Log.cpp
@@ -31,7 +31,7 @@ std::string ConvertString(const std::wstring& str)
 		return std::string();
 	}
 	std::string result(sizeNeeded, 0);
-	//WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
 	return result;
 }
 
