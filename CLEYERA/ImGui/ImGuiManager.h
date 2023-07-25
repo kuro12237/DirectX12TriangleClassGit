4591cea3d1988537061b8fee8bba3038d3c352f4
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-07-25 22:12:46 +0900

    02_00_確認課題

--- CLEYERA/ImGui/ImGuiManager.h
+++ CLEYERA/ImGui/ImGuiManager.h
@@ -1,9 +1,9 @@
 #pragma once
 #include"DirectXSetup.h"
 #include"WinApp.h"
-#include"../externals/imgui/imgui.h"
-#include"../externals/imgui/imgui_impl_dx12.h"
-#include"../externals/imgui/imgui_impl_win32.h"
+#include"imgui.h"
+#include"imgui_impl_dx12.h"
+#include"imgui_impl_win32.h"
 
 class ImGuiManager
 {
