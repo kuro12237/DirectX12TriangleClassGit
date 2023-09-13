1a13b4198a76d903c870cb141fb22a200b7c0e7f
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-09-13 17:02:35 +0900

    s

--- CLEYERA/Base/Win/WinApp.h
+++ CLEYERA/Base/Win/WinApp.h
@@ -9,7 +9,7 @@ public:
 
 	static void Initialize();
 
-	static void Msg(MSG& msg);
+	static bool WinMsg();
 
 	static void Finalize();
 
@@ -24,18 +24,31 @@ public:
 
 private:
 
+	static HWND CreateWIND(const int32_t kWidth, const int32_t kHeight,LPCTSTR title,WNDCLASS wc);
+
 	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
 	static const int32_t kWindowWidth = 1280;
 	static const int32_t kWindowHeight = 720;
+
+	static const int32_t kDebugToolWindowWidth = 320;
+	static const int32_t kDebugToolWindowHeight = 130;
+
+	MSG msg{};
+
+
 	WNDCLASS wc_{};
 	HWND hwnd_ = {};
+
+	HWND DebugHwnd_{};
+
+#pragma region ƒVƒ“ƒOƒ‹
 
 	WinApp() = default;
 	~WinApp() = default;
 	WinApp(const WinApp&) = delete;
 	const WinApp& operator=(const WinApp&) = delete;
-
+#pragma endregion
 };
 
 
