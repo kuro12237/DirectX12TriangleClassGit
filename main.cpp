5755204c7361b56b2ac01f213ef2211aae1f684e
kuro12237 <115063113+kuro12237@users.noreply.github.com>
2023-09-19 05:58:02 +0900

    save

--- main.cpp
+++ main.cpp
@@ -1,8 +1,7 @@
 #include"GameManager.h"
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
-{
-	GameManager* gameManager_ = new GameManager();
+{	GameManager* gameManager_ = new GameManager();
 	gameManager_->Run();
 	delete gameManager_;
 	return 0;
